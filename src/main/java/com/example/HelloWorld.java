806002fcd27e4ba7d0a7a0e2d33ec7a8b2c6776b
dankirichok1 <dan.kirichok@angi.com>
2025-08-19 23:48:56 -0400

    Refactor HelloWorld.java with best practices
    - Moved to proper Maven directory structure (src/main/java)
    - Added package declaration (com.example)
    - Added comprehensive JavaDoc documentation
    - Fixed missing newline at end of file
    - Enhanced code organization and readability

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.example;
+
+/**
+ * A simple Hello World program for demonstration purposes.
+ * Created as part of example-ticket-1.
+ * 
+ * @author dankirichok1
+ * @version 1.0
+ */
+public class HelloWorld {
+    
+    /**
+     * Main method that prints greeting messages.
+     * 
+     * @param args command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is an example Java program for example-ticket-1");
+    }
+}
