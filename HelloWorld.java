974624fb8bd95b088be59a9d04b7b2be9c16e218
dankirichok1 <dan.kirichok@angi.com>
2025-08-19 23:49:42 -0400

    Remove old HelloWorld.java from root
    - Moved to proper Maven directory structure (src/main/java/com/example/)
    - Maintaining clean project organization

--- HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("This is an example Java program for example-ticket-1");
-    }
-}
