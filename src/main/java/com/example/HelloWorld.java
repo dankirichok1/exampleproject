8276395523676262401fe34aed0a67b218520715
dankirichok1 <dan.kirichok@angi.com>
2025-08-20 12:15:10 -0400

    Expand HelloWorld with multiple variables and Hello World output
    - Added 6 "Hello World" print statements
    - Created 10 integer variables (a-j) each with value 123
    - Added sum calculation of all 10 variables (total: 1230)
    - Enhanced program to demonstrate multiple variable addition

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,13 +2,24 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Integer a = 123;
-        Integer b = 456;
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
 
-        System.out.println(a + b);
-        System.out.println(a + b);
-        System.out.println(a + b);
-        System.out.println(a + b);
-        System.out.println(a + b);
+        int a = 123;
+        int b = 123;
+        int c = 123;
+        int d = 123;
+        int e = 123;
+        int f = 123;
+        int g = 123;
+        int h = 123;
+        int i = 123;
+        int j = 123;
+
+        System.out.println(a + b + c + d + e + f + g + h + i + j);
     }
 }
