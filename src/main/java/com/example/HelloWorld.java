d71d47d309272aa8330cc5ccd3349d818d3d57d8
dankirichok1 <dan.kirichok@angi.com>
2025-08-20 00:00:44 -0400

    Update HelloWorld to perform integer addition
    - Added integer variables a=123 and b=456
    - Changed output to print sum (579) five times
    - Simplified code structure

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,21 +1,14 @@
 package com.example;
 
-/**
- * A simple Hello World program for demonstration purposes.
- * Created as part of example-ticket-1.
- * 
- * @author dankirichok1
- * @version 1.0
- */
 public class HelloWorld {
-    
-    /**
-     * Main method that prints greeting messages.
-     * 
-     * @param args command line arguments (not used)
-     */
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("This is an example Java program for example-ticket-1");
+        Integer a = 123;
+        Integer b = 456;
+
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
     }
 }
